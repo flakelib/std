d614857f99c79a32e30ab190c0eb1bac0b1164e4
arcnmx <arcnmx@users.noreply.github.com>
2022-04-12 18:44:39 -0700

    flake checks

--- flake.nix
+++ flake.nix
@@ -1,8 +1,15 @@
 {
   description = "No-nixpkgs standard library for the Nix expression language";
 
-  outputs = { self }:
-    {
+  outputs = { self }: let
+    systems = [ "x86_64-linux" ];
+    forEachSystem = f: self.lib.set.fromList (
+      self.lib.list.map (system: { _0 = system; _1 = f system; }) systems
+    );
+  in {
       lib = import ./default.nix;
+      checks = forEachSystem (system: {
+        test = import ./test { inherit system; };
+      });
     };
 }
