e8266aad250ebdf496c9ea8b279848265aad349d
arcnmx <arcnmx@users.noreply.github.com>
2022-05-25 14:55:57 -0700

    drv passthru

--- drv.nix
+++ drv.nix
@@ -1,5 +1,5 @@
 { lib }: let
-  inherit (lib) Str Path Ty Nix Drv;
+  inherit (lib) Str Path Ty Null Nix Drv;
 in {
   isPath = p: Str.hasPrefix Nix.storeDir p;
 
@@ -18,4 +18,21 @@ in {
 
   # absolute path to a derivation's `mainProgramName`
   mainProgram = d: "${d.bin or d}/bin/${Drv.mainProgramName d}";
+
+  appendPassthru = passthru: d: d // passthru; # TODO: nixpkgs.lib.extendDerivation also modifies outputs
+
+  # derivations that can reference their own (potentially overridden) attributes
+  Fix = fn: let
+    drv = fn drv;
+    passthru = {
+      ${Null.Iif (drv ? override) "override"} = f: Drv.Fix (drv: (fn drv).override f);
+      ${Null.Iif (drv ? overrideDerivation) "overrideDerivation"} = f: Drv.Fix (drv: (fn drv).overrideDerivation f);
+      ${Null.Iif (drv ? overrideAttrs) "overrideAttrs"} = f: Drv.Fix (drv: (fn drv).overrideAttrs f);
+    };
+  in Drv.appendPassthru passthru drv;
+
+  # add persistent passthru attributes that can refer to the derivation
+  fixPassthru = fn: drv: if Ty.function.check drv # allow chaining with mkDerivation
+    then attrs: Drv.fixPassthru fn (drv attrs)
+    else Drv.Fix (dself: drv.overrideAttrs (old: { passthru = old.passthru or {} // fn dself; }));
 }
