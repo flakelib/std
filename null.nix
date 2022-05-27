5ad8b6f54b32f3a91062477d1d53688fd9a2b221
arcnmx <arcnmx@users.noreply.github.com>
2022-05-27 14:30:48 -0700

    {Opt,Null}.default

--- null.nix
+++ null.nix
@@ -1,7 +1,12 @@
 { lib }: let
-  inherit (lib) Null Bool Ty;
+  inherit (lib) Null Bool Fn Ty;
 in {
   inherit (Null.functor) map;
+
+  default = d: Fn.flip Null.match {
+    just = Fn.id;
+    nothing = d;
+  };
 
   Iif = Bool.toNullable;
 
--- opt.nix
+++ opt.nix
@@ -1,7 +1,12 @@
 { lib }: let
-  inherit (lib) Opt Bool Ty;
+  inherit (lib) Opt Bool Fn Ty;
 in {
   inherit (Opt.functor) map;
+
+  default = d: Fn.flip Opt.match {
+    just = Fn.id;
+    nothing = d;
+  };
 
   Iif = Bool.toOptional;
 
